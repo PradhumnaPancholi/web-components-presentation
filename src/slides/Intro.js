c4bfb2d6d9983989250ec891ec30e5a913afb1f7
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2021-01-12 17:41:35 -0500

    added intro

---
+++ src/slides/Intro.js
@@ -0,0 +1,11 @@
+import {Heading, Text} from 'spectacle'
+import React from 'react'
+
+export const Intro = () => {
+    return(
+        <div>
+            <Heading>Web Components</Heading>
+            <Text>A set of web platform API's that allow us to create custom, reusable, and encapsulated html tags to use in web pages/web apps.</Text>
+        </div>
+    )
+}
